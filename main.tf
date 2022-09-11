99b85f2d37d6b008e3e2b0fdfdaec696b7ca81c2
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-09-11 13:13:30 +0500

    delete storage account from main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
 
 resource "azurerm_resource_group" "rg-learntf-weu-dev" {
-  name     = "rg-learntftf-weu-dev"
+  name     = "rg-learntf-weu-dev"
   location = "West Europe"
-}
-
-data "azurerm_storage_account" "stolearntftfweudev" {
-  name                = "stolearntftfweudev"
-  resource_group_name = azurerm_resource_group.rg-learntf-weu-dev.name
-#   account_tier        = "Standard"
-}
-
-output "storage_account_tier" {
-  value = data.azurerm_storage_account.stolearntftfweudev.account_tier
 }
