a1ffad048e63694694ae372d8ac796cd989286b6
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-09-17 07:36:48 +0500

    complete modules for platform

--- module-iac/output.tf
+++ module-iac/output.tf
@@ -17,3 +17,7 @@ output "access_key" {
   sensitive = true
 
 }
+
+output "account_id" {
+  value = data.azurerm_client_config.current.client_id
+}
--- module-iac/providers.tf
+++ module-iac/providers.tf
@@ -2,3 +2,14 @@ provider "azurerm" {
   features {}
 
 }
+
+# terraform {
+#   required_providers {
+#     name = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>2.0"
+#     }
+#   }
+# }
+
+data "azurerm_client_config" "current" {}
